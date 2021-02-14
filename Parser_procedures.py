3f44665ed547f4b76f610b81b2225023e651d41f
Totenkaf <61432398+Totenkaf@users.noreply.github.com>
2021-02-14 23:45:20 +0300

    Delete Parser_procedures.py

--- Parser_procedures.py
+++
@@ -1,3 +0,0 @@
-def filePreparation(filename):
-    names = ["Addr", "Command", "Pressure", "Time", "Date"]
-    sensor_surf = pd.read_csv(filename, skiprows = 2, header=None, delimiter=';', names=names)
